e838d2102fbbe1d305284afdfcce79a1410f2d6b
Christian Foster <fosterchris000@gmail.com>
2022-02-26 13:41:21 -0500

    switched from .then syntax to async await. getNFTData now returns an object with endDate, info, and price properties.

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 const Nightmare = require("nightmare");
 const nightmare = Nightmare();
 
-const getNFTdata = (suffix) => {
-  nightmare
-    .goto("https://opensea.io/assets/" + suffix)
+const base_url = "https://opensea.io/assets/";
+
+const getNFTdata = async (suffix) => {
+  const nftData = await nightmare
+    .goto(base_url + suffix)
     .wait(
       "div.TradeStation--main > div.TradeStation--price-container > div.TradeStation--price > div.Price--amount"
     )
-    .evaluate(
-      () =>
-        document.querySelector(
+    .wait("div.TradeStation--header > div > div > div > span:nth-child(2)")
+    .evaluate(() => {
+      return {
+        price: +document.querySelector(
           "div.TradeStation--main > div.TradeStation--price-container > div.TradeStation--price > div.Price--amount"
-        ).innerText
-    )
-    .end()
-    .then((price) => {
-      console.log("NFT price", price);
-      return;
+        ).innerText,
+        endDate: document.querySelector(
+          "div.TradeStation--header > div > div > div> span:nth-child(2)"
+        ).innerText,
+        info: "Info Property",
+      };
     })
-    .catch((error) => {
-      console.error("Search failed:", error);
-    });
+    .end();
+  return nftData;
 };
 
-const nftURLSuffix = "0xb4d06d46a8285f4ec79fd294f78a881799d8ced9/6838";
+const fetchData = async () => {
+  const nftURLSuffix = "0xb4d06d46a8285f4ec79fd294f78a881799d8ced9/2940";
+  const full_url = base_url + nftURLSuffix;
+  console.log(`Fetch data for: \n  ${full_url}`);
+  const nftAuctionData = await getNFTdata(nftURLSuffix);
+  console.log("nftAuctionData :", nftAuctionData);
+};
 
-const nftAuctionPrice = getNFTdata(nftURLSuffix);
+fetchData();
